594648040adb9bc7bee10bc3e1c2acea9ddd1c90
Carlos Aguirre <38355125+CarlosAguirreV@users.noreply.github.com>
2020-12-10 08:28:35 +0100

    Añadida la página de Aplicaciones.
    Ahora se puede acceder a la página de aplicaciones.

--- recursos/javascript/controlador.js
+++ recursos/javascript/controlador.js
@@ -7,7 +7,12 @@ var contraerApp = true;
 
 function main(){
 	
-	// ################## FUNCIONES ##################
+	// ################## EVENTOS ##################
+	// Al cargar la pagina cambiara el fondo en funcion de la estacion del anio.
+	$(document).ready(function(){
+		setFondoSegunEstacion();
+	});
+	
 	//Evitar el uso del menú contextual (clic derecho).
 	document.oncontextmenu = function(){return false}
 	
@@ -27,30 +32,26 @@ function main(){
 	});
 	
 	// Cambia la opacidad del logo si se pasa el raton por encima.
-	$('#logo')
+	$('.logo')
 	.mouseenter(function(){
-		$('#logo').animate({
-			opacity: '0.5'
-		}).delay("fast");
+		$(".logo").attr("src","recursos/imagenes/logo2.png");
 	})
 	.mouseleave(function(){
-		$('#logo').animate({
-			opacity: '1'
-		}).delay("fast");
+		$(".logo").attr("src","recursos/imagenes/logo1.png");
 	});
 	
 	// Contraer o expandir juegos.
-	$('.btn_contraer_juegos').click(function(){
+	$('.btn_contraer_pc').click(function(){
 		if(contraerJuegos){
 			contraerJuegos = false;
-			$('.contractor_juegos').hide(200);
-			$(".btn_contraer_juegos span").removeClass("icon-circle-up");
-			$(".btn_contraer_juegos span").addClass("icon-circle-down");
+			$('.contractor_pc').hide(200);
+			$(".btn_contraer_pc span").removeClass("icon-circle-up");
+			$(".btn_contraer_pc span").addClass("icon-circle-down");
 		} else {
 			contraerJuegos = true;
-			$('.contractor_juegos').show(200);
-			$(".btn_contraer_juegos span").removeClass("icon-circle-down");
-			$(".btn_contraer_juegos span").addClass("icon-circle-up");
+			$('.contractor_pc').show(200);
+			$(".btn_contraer_pc span").removeClass("icon-circle-down");
+			$(".btn_contraer_pc span").addClass("icon-circle-up");
 		}
 	});
 	
@@ -67,5 +68,35 @@ function main(){
 			$(".btn_contraer_app span").addClass("icon-circle-up");
 		}
 	});
+	
+	
+	
+	// ################## FUNCIONES ##################
+	// 0 Primavera - 21 marzo(3) al 20 junio(6).
+	// 1 Verano - 21 junio(6) al 22 septiembre(9).
+	// 2 Otonio - 22 septiembre(9) al 20 diciembre(12).
+	// 3 Invierno - 21 diciembre(12) al 20 de marzo(3).
+	function setFondoSegunEstacion(){
+		var fecha = new Date();
+		var mes = fecha.getMonth() + 1;
+		
+		// Primavera
+		if(mes > 3 && mes < 7){
+			$("body").css("background","#518d5e");
+			$("body").css('background-image','url("recursos/imagenes/bg_primavera.png")');
+		// Verano
+		}else if(mes > 6 && mes < 10){
+			$("body").css("background","#05b7ff");
+			$("body").css('background-image','url("recursos/imagenes/bg_verano.png")');
+		// Otonio
+		}else if(mes > 9){
+			$("body").css("background","#b58550");
+			$("body").css('background-image','url("recursos/imagenes/bg_otonio.png")');
+		// Invierno
+		}else{
+			$("body").css("background","#aeabb1");
+			$("body").css('background-image','url("recursos/imagenes/bg_invierno.png")');
+		}
+	}
 	
 };
